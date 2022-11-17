d0a4f32b6d69fee2dd150503e86d2807d8713477
Rawle Juglal <rawledjuglal@gmail.com>
2022-11-16 18:47:30 -0600

    change
    imageUrl to reflect /TravelJournal for deployment

--- src/data.js
+++ src/data.js
@@ -7,7 +7,7 @@ export default[
         startDate:'10 May, 2014',
         endDate:'17 May, 2014',
         description:'Sydney is the capital city of the state of New South Wales, and the most populous city in both Australia and Oceania.Located on Australia\'s east coast, the metropolis surrounds Sydney Harbour and extends about 70 km (43.5 mi) towards the Blue Mountains to the west, Hawkesbury to the north, the Royal National Park to the south and Macarthur to the south-west',
-        imageUrl:"/images/SydneyOperaHouse.jpg"
+        imageUrl:"/TravelJournal/images/SydneyOperaHouse.jpg"
     },
     {
         id:2,
@@ -17,7 +17,7 @@ export default[
         startDate:'18 May, 2014',
         endDate:'24 May, 2014',
         description:'Trinity Beach is approximately 20 kilometres (10 mi) from the Cairns city centre and approximately 6.6 km from Smithfield. Trinity Beach is also a beach stretching for about a mile on the eastern coast of Australia bounded by the Coral Sea.[3] The suburb itself is approximately 2 km wide.',
-        imageUrl:'/images/TrinityBeach.jpg' 
+        imageUrl:'/TravelJournal/images/TrinityBeach.jpg' 
     },
     {
         id:3,
@@ -27,7 +27,7 @@ export default[
         startDate:'9 Sep, 2018',
         endDate:'12 Sep, 2018',
         description:'Reykjavik, on the coast of Iceland, is the country\'s capital and largest city. It\'s home to the National and Saga museums, tracing Iceland’s Viking history. The striking concrete Hallgrimskirkja church and rotating Perlan glass dome offer sweeping views of the sea and nearby hills. Exemplifying the island’s volcanic activity is the geothermal Blue Lagoon spa, near the village of Grindavik.',
-        imageUrl:'/images/ReykjavikIceland.jpg' 
+        imageUrl:'/TravelJournal/images/ReykjavikIceland.jpg' 
     },
     {
         id:4,
@@ -37,7 +37,7 @@ export default[
         startDate:'12 Sep, 2018',
         endDate:'14 Sep, 2018',
         description:'London, the capital of England and the United Kingdom, is a 21st-century city with history stretching back to Roman times. At its centre stand the imposing Houses of Parliament, the iconic ‘Big Ben’ clock tower and Westminster Abbey, site of British monarch coronations. Across the Thames River, the London Eye observation wheel provides panoramic views of the South Bank cultural complex, and the entire city.',
-        imageUrl:'/images/LondonEngland.jpg' 
+        imageUrl:'/TravelJournal/images/LondonEngland.jpg' 
     },
     {
         id:5,
@@ -47,7 +47,7 @@ export default[
         startDate:'14 Sep, 2018',
         endDate:'16 Sep,2018',
         description:'Manchester is a major city in the northwest of England with a rich industrial heritage. The Castlefield conservation area’s 18th-century canal system recalls the city’s days as a textile powerhouse, and visitors can trace this history at the interactive Museum of Science & Industry. The revitalised Salford Quays dockyards now house the Daniel Libeskind-designed Imperial War Museum North and the Lowry cultural centre.',
-        imageUrl:'/images/ManchesterEngland.jpg' 
+        imageUrl:'/TravelJournal/images/ManchesterEngland.jpg' 
     },
     {
         id:6,
@@ -57,7 +57,7 @@ export default[
         startDate:'15 Jun, 2021',
         endDate:'21 Jul, 2021',
         description:'Tamarindo is a town in the Guanacaste Province, on Costa Rica’s Pacific coast. It’s known for beaches with strong surf, like Playa Tamarindo and Playa Langosta. To the north, Playa Grande beach is a major nesting site for huge leatherback turtles, and forms part of Las Baulas National Marine Park. The mangrove-lined estuary of Tamarindo Wildlife Refuge protects animals such as howler monkeys and crocodiles.',
-        imageUrl:'/images/TamarindoCostaRica.jpg' 
+        imageUrl:'/TravelJournal/images/TamarindoCostaRica.jpg' 
     },
     {
         id:7,
@@ -67,7 +67,7 @@ export default[
         startDate:'22 Jul, 2021',
         endDate:'15 Aug, 2021',
         description:'Jacó is a town on the Pacific coast of Costa Rica, southwest of the capital city, San José. It\'s known for its surf beaches and nightlife, and as a gateway to national parks. Rocky, gray-sand Jacó Beach is west of town. Southeast, Hermosa Beach has big waves. To the north, Carara National Park has crocodiles at Tárcoles River and scarlet macaws. A path through rainforest in Pura Vida park leads to Bijagual Waterfall.',
-        imageUrl:'/images/JacoCostaRica.jpg' 
+        imageUrl:'/TravelJournal/images/JacoCostaRica.jpg' 
     },
     {
         id:8,
@@ -77,7 +77,7 @@ export default[
         startDate:'16 Aug, 2021',
         endDate:'16 Aug, 2021',
         description:'Panama City is a city in and the county seat of Bay County, Florida, United States. Located along U.S. Highway 98, it is the largest city between Tallahassee and Pensacola. It is the more populated city of the Panama City–Lynn Haven, Florida metropolitan statistical area.',
-        imageUrl:'/images/PanamaCityPanama.jpg' 
+        imageUrl:'/TravelJournal/images/PanamaCityPanama.jpg' 
     },
     {
         id:9,
@@ -87,7 +87,7 @@ export default[
         startDate:'19 Aug, 2021',
         endDate:'19 Aug, 2021',
         description:'Munich, Bavaria’s capital, is home to centuries-old buildings and numerous museums. The city is known for its annual Oktoberfest celebration and its beer halls, including the famed Hofbräuhaus, founded in 1589. In the Altstadt (Old Town), central Marienplatz square contains landmarks such as Neo-Gothic Neues Rathaus (town hall), with a popular glockenspiel show that chimes and reenacts stories from the 16th century.',
-        imageUrl:'/images/MunichGermany.jpg' 
+        imageUrl:'/TravelJournal/images/MunichGermany.jpg' 
     },
     {
         id:10,
@@ -97,7 +97,7 @@ export default[
         startDate:'20 Aug, 2021',
         endDate:'15 Sep, 2021',
         description:'Tirana, the capital of Albania, is known for its colorful Ottoman-, Fascist- and Soviet-era architecture. Pastel buildings surround the city\'s focal point, Skanderbeg Square, which is named for its equestrian statue of a national hero. On the square\'s north end is the modernist National History Museum, covering prehistoric times through Communist rule and the anti-Communist uprisings of the 1990s.',
-        imageUrl:'/images/TiranaAlbania.jpg' 
+        imageUrl:'/TravelJournal/images/TiranaAlbania.jpg' 
     },
     {
         id:11,
@@ -107,7 +107,7 @@ export default[
         startDate:'16 Sep, 2021',
         endDate:'23 Sep, 2021',
         description:'Dubrovnik is a city in southern Croatia fronting the Adriatic Sea. It\'s known for its distinctive Old Town, encircled with massive stone walls completed in the 16th century. Its well-preserved buildings range from baroque St. Blaise Church to Renaissance Sponza Palace and Gothic Rector’s Palace, now a history museum. Paved with limestone, the pedestrianized Stradun (or Placa) is lined with shops and restaurants.',
-        imageUrl:'/images/DubrovnikCroatia.jpg' 
+        imageUrl:'/TravelJournal/images/DubrovnikCroatia.jpg' 
     },
     {
         id:12,
@@ -117,7 +117,7 @@ export default[
         startDate:'23 Sep, 2021',
         endDate:'1 Oct, 2021',
         description:'Split is the second-largest city of Croatia, the largest city in Dalmatia and the largest city on the Croatian coast. It lies on the eastern shore of the Adriatic Sea and is spread over a central peninsula and its surroundings.',
-        imageUrl:'/images/SplitCroatia.jpg' 
+        imageUrl:'/TravelJournal/images/SplitCroatia.jpg' 
     },
     {
         id:13,
@@ -127,7 +127,7 @@ export default[
         startDate:'1 Oct, 2021',
         endDate:'12 Oct, 2021',
         description:'Rome is the capital city of Italy. It is also the capital of the Lazio region, the centre of the Metropolitan City of Rome, and a special comune named Comune di Roma Capitale.',
-        imageUrl:'/images/RomeItaly.jpg' 
+        imageUrl:'/TravelJournal/images/RomeItaly.jpg' 
     },
     {
         id:14,
@@ -137,7 +137,7 @@ export default[
         startDate:'13 Oct, 2021',
         endDate:'22 Oct, 2021',
         description:'Milan, a metropolis in Italy\'s northern Lombardy region, is a global capital of fashion and design. Home to the national stock exchange, it’s a financial hub also known for its high-end restaurants and shops. The Gothic Duomo di Milano cathedral and the Santa Maria delle Grazie convent, housing Leonardo da Vinci’s mural “The Last Supper,” testify to centuries of art and culture.',
-        imageUrl:'/images/MilanItaly.jpg' 
+        imageUrl:'/TravelJournal/images/MilanItaly.jpg' 
     },
     {
         id:15,
@@ -147,7 +147,7 @@ export default[
         startDate:'19 Oct, 2021',
         endDate:'20 Oct, 2021',
         description:'Venice, the capital of northern Italy’s Veneto region, is built on more than 100 small islands in a lagoon in the Adriatic Sea. It has no roads, just canals – including the Grand Canal thoroughfare – lined with Renaissance and Gothic palaces. The central square, Piazza San Marco, contains St. Mark’s Basilica, which is tiled with Byzantine mosaics, and the Campanile bell tower offering views of the city’s red roofs.',
-        imageUrl:'/images/VeniceItaly.jpg' 
+        imageUrl:'/TravelJournal/images/VeniceItaly.jpg' 
     },
     {
         id:16,
@@ -157,7 +157,7 @@ export default[
         startDate:'23 Oct, 2021',
         endDate:'26 Oct, 2021',
         description:'Lisbon is Portugal’s hilly, coastal capital city. From imposing São Jorge Castle, the view encompasses the old city’s pastel-colored buildings, Tagus Estuary and Ponte 25 de Abril suspension bridge. Nearby, the National Azulejo Museum displays 5 centuries of decorative ceramic tiles. Just outside Lisbon is a string of Atlantic beaches, from Cascais to Estoril.',
-        imageUrl:'/images/LisbonPortugal.jpg' 
+        imageUrl:'/TravelJournal/images/LisbonPortugal.jpg' 
     },
     {
         id:17,
@@ -167,6 +167,6 @@ export default[
         startDate:'27 Oct, 2021',
         endDate:'3 Nov, 2021',
         description:'Paris, France\'s capital, is a major European city and a global center for art, fashion, gastronomy and culture. Its 19th-century cityscape is crisscrossed by wide boulevards and the River Seine. Beyond such landmarks as the Eiffel Tower and the 12th-century, Gothic Notre-Dame cathedral, the city is known for its cafe culture and designer boutiques along the Rue du Faubourg Saint-Honoré.',
-        imageUrl:'/images/ParisFrance.jpg' 
+        imageUrl:'/TravelJournal/images/ParisFrance.jpg' 
     }
 ]
